03e6d7a695d96d89740479e63801e3a688eaf5aa
jeonyoungho <yhjun1000@gmail.com>
2022-01-30 19:11:43 +0900

    UserList 컴포넌트 User 컴포넌트 추가 제거 수정 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,39 +1,80 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
 
-function User({ user }) {
-    return (
-        <div>
-            <b>{user.username}</b> <span>({user.email})</span>
-        </div>
-    );
-}
-
-function UserList() {
-    const users = [
+function App() {
+    const [inputs, setInputs] = useState({
+        username: '',
+        email: ''
+    });
+    const { username, email } = inputs;
+    const onChange = e => {
+        const { name, value } = e.target;
+        setInputs({
+            ...inputs,
+            [name]: value
+        });
+    };
+    const [users, setUsers] = useState([
         {
             id: 1,
             username: 'velopert',
-            email: 'public.velopert@gmail.com'
+            email: 'public.velopert@gmail.com',
+            active: true
         },
         {
             id: 2,
             username: 'tester',
-            email: 'tester@example.com'
+            email: 'tester@example.com',
+            active: false
         },
         {
             id: 3,
             username: 'liz',
-            email: 'liz@example.com'
+            email: 'liz@example.com',
+            active: false
         }
-    ];
+    ]);
 
+    const nextId = useRef(4);
+    const onCreate = () => {
+        const user = {
+            id: nextId.current,
+            username,
+            email
+        };
+        setUsers(users.concat(user));
+
+        setInputs({
+            username: '',
+            email: ''
+        });
+        nextId.current += 1;
+    };
+
+    const onRemove = id => {
+        // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+        // = user.id 가 id 인 것을 제거함
+        setUsers(users.filter(user => user.id !== id));
+    };
+    const onToggle = id => {
+        setUsers(
+            users.map(user =>
+                user.id === id ? { ...user, active: !user.active } : user
+            )
+        );
+    };
     return (
-        <div>
-            {users.map(user => (
-                <User user={user} key={user.id} />
-            ))}
-        </div>
+        <>
+            <CreateUser
+                username={username}
+                email={email}
+                onChange={onChange}
+                onCreate={onCreate}
+            />
+            <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+        </>
     );
 }
 
-export default UserList;
+export default App;
