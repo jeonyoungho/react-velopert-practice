310f6d70255b422909d7b3c51f327e006287894f
jeonyoungho <yhjun1000@gmail.com>
2022-02-02 19:54:56 +0900

    Context API 적용

--- src/CreateUser.js
+++ src/CreateUser.js
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, {useCallback, useContext, useRef} from 'react';
+import {UserDispatch} from "./App";
+import useInputs from "./hooks/useInputs";
 
-const CreateUser = ({ username, email, onChange, onCreate }) => {
+const CreateUser = () => {
+
+    const dispatch = useContext(UserDispatch);
+
+    const [{ username, email }, onChange, onReset] = useInputs({
+        username: '',
+        email: ''
+    });
+
+    const nextId = useRef(4);
+
+    const onCreate = useCallback(() => {
+        dispatch({
+            type: 'CREATE_USER',
+            user: {
+                id: nextId.current,
+                username,
+                email
+            }
+        });
+        onReset();
+        nextId.current += 1;
+    }, [username, email, onReset]);
+
     return (
         <div>
             <input
