d76b9d89dbcee95e95efab12f16d4517f2c65d81
jeonyoungho <yhjun1000@gmail.com>
2022-03-06 19:09:53 +0900

    useAsync 커스텀 Hook 적용

--- api-integrate/src/App.js
+++ api-integrate/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Users } from './Users';
+import Users from './Users';
 
 function App() {
   return <Users/>
--- api-integrate/src/Users.js
+++ api-integrate/src/Users.js
@@ -1,83 +1,44 @@
 import React, { useState, useEffect, useReducer } from 'react';
 import axios from 'axios';
+import useAsync from './useAsync';
+import User from './User';
 
-function reducer(state, action) {
-  switch(action.type) {
-    case 'LOADING':
-      return {
-        loading: true,
-        data: null,
-        error: null
-      };
-    case 'SUCCESS':
-      return {
-        loading: false,
-        data: action.data,
-        error: null
-      }
-    case "ERROR":
-      return {
-        loading: false,
-        data: null,
-        error: action.error
-      }
-    default:
-      throw new Error(`Unhandled action type: ${action.type}`);
-  }
+// useAsync 에서는 Promise 의 결과를 바로 data 에 담기 때문에,
+// 요청을 한 이후 response 에서 data 추출하여 반환하는 함수를 따로 만들었습니다.
+async function getUsers() {
+  const response = await axios.get(
+    'https://jsonplaceholder.typicode.com/users'
+  );
+  return response.data;
 }
 
 function Users() {
-  /*
-    useReducer 로 구현했을 때의 장점은 useState 의 setState 함수를 여러번 사용하지 않아도 된다는점과,
-    리듀서로 로직을 분리했으니 다른곳에서도 쉽게 재사용을 할 수 있다는 점 이다.
-    물론, 취향에 따라 useState 로 구현을 해도 무방하다.
-  */
-  const [state, dispatch] = useReducer(reducer, {
-    loading: false,
-    data: null,
-    error: null
-  });
+  const [userId, setUserId] = useState(null);
+  const [state, refetch] = useAsync(getUsers, [], true);
 
-  const fetchUsers = async () => {
-    dispatch({ type: 'LOADING'});
-    try {
-      const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/users'
-      );
-      dispatch({
-        type: 'SUCCESS',
-        data: response.data
-      });
-    } catch (e) {
-      dispatch({
-        type: 'ERROR',
-        error: e
-      });
-    }
-  };
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
-  const { loading, data: users, error} = state; // state.data 를 users 키워드로 조회
+  const { loading, data: users, error } = state; // state.data 를 users 키워드로 조회
 
   if (loading) return <div>로딩중...</div>;
   if (error) return <div>에러가 발생했습니다.</div>;
-  if (!users) return null;
+  if (!users) return <button onClick={refetch}>불러오기</button>;
 
   return (
     <>
       <ul>
         {users.map(user => (
-          <li key={user.id}>
+          <li
+            key={user.id}
+            onClick={() => setUserId(user.id)}
+            style={{cursor: 'pointer'}}
+          >
             {user.username} ({user.name})
           </li>
         ))}
       </ul>
-      <button onClick={fetchUsers}>다시 불러오기</button>
+      <button onClick={refetch}>다시 불러오기</button>
+      {userId && <User id={userId} />}
     </>
   );
 }
 
-export {Users};
+export default Users;
