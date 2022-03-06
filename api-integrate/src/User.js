4a135dcf18a850125bbb689af9c96b8223a8c753
jeonyoungho <yhjun1000@gmail.com>
2022-03-06 19:26:54 +0900

    react-async 라이브러리를 활용하여 요청 상태 관리하도록 변경

--- api-integrate/src/User.js
+++ api-integrate/src/User.js
@@ -1,19 +1,27 @@
 import React from "react";
 import axios from "axios";
-import useAsync from './useAsync';
+import { useAsync } from 'react-async';
 
-async function getUser(id) {
+async function getUser({ id }) {
   const response = await axios.get(
     `https://jsonplaceholder.typicode.com/users/${id}`
   );
   return response.data;
 }
 
-function User({id}) {
-  const [state] = useAsync(() => getUser(id), [id]);
-  const { loading, data: user, error } = state;
+function User({ id }) {
+  const {data: user, error, isLoading } = useAsync({
+    promiseFn: getUser,
+    id,
+    watch: id
+  });
 
-  if (loading) return <div>로딩중..</div>;
+  /*
+    useAsync 를 사용 할 때 watch 값에 특정 값을 넣어주면
+    이 값이 바뀔 때마다 promiseFn 에 넣은 함수를 다시 호출해준다.
+   */
+
+  if (isLoading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
   if (!user) return null;
 
