7694cce06ba2a3a31c17dc3df54e3a4c405ba114
jeonyoungho <yhjun1000@gmail.com>
2022-03-12 12:45:59 +0900

    리액트 컴포넌트 타입스크립트로 작성하기 섹션 관련 개발 내용 추가

--- ts-react-tutorial/src/Greetings.tsx
+++ ts-react-tutorial/src/Greetings.tsx
@@ -1,19 +1,27 @@
-// @ts-ignore
 import React from 'react';
 
 type GreetingsProps = {
-    name: string;
-    mark: string;
+  name: string;
+  mark: string;
+  optional?: string;
+  onClick: (name: string) => void; // 아무것도 리턴하지 않는다는 함수를 의미합니다.
 };
 
-const Greetings: React.FC<GreetingsProps> = ({ name, mark }) => (
+function Greetings({ name, mark, optional, onClick }: GreetingsProps) {
+  const handleClick = () => onClick(name);
+  return (
     <div>
-        Hello, {name} {mark}
+      Hello, {name} {mark}
+      {optional && <p>{optional}</p>}
+      <div>
+        <button onClick={handleClick}>Click Me</button>
+      </div>
     </div>
-);
+  );
+}
 
 Greetings.defaultProps = {
-    mark: '!'
+  mark: '!'
 };
 
 export default Greetings;
